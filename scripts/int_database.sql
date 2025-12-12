6fab3fa2eb4e631010a3914df9bf77b568b9dd2d
Adlane_BEY <95702979+bey-adlane@users.noreply.github.com>
2025-12-12 22:39:30 +0100

    Create DataWarehouse database and schemas
    This script creates a new database named 'DataWarehouse' and three schemas: 'bronze', 'silver', and 'gold'. It also includes a warning about data loss if the database already exists.

--- scripts/int_database.sql
+++ scripts/int_database.sql
@@ -1,1 +1,48 @@
+/*
+=============================================================
+Création de la Base de Données et des Schémas
+=============================================================
+Objectif du Script :
+    Ce script crée une nouvelle base de données nommée 'DataWarehouse' après avoir vérifié son existence.
+    Et il crée aussi le script configure trois schémas au sein de cette base de données : 
+    'bronze', 'silver' et 'gold'.
 
+	
+⚠️ Avertissement
+ATTENTION :
+
+L'exécution de ce script supprimera l'intégralité de la base de données 'DataWarehouse' si elle existe.
+
+Toutes les données contenues dans cette base de données seront définitivement effacées.
+
+Veuillez procéder avec prudence et assurez-vous de disposer de sauvegardes appropriées avant d'exécuter ce script.
+*/
+
+USE master;
+GO
+
+--Supprimer et recréer la base de données 'DataWarehouse'
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'datawarehouse')
+BEGIN
+	AlTER DATABASE Datawarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE
+	DROP DATABASE Datawarehouse
+END;
+GO
+
+-- Creation de la base de données 'Datawarehouse'
+CREATE DATABASE Datawarehouse;
+GO
+
+USE Datawarehouse;
+GO
+
+
+-- Creation Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
